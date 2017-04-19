f17c70dc7ebda8b9790ce61c550ef189d7e3e2a9
LL117394 <okman999@aliyun.com>
2017-04-19 11:04:08 +0800

    update

--- app/src/main/java/com/che58/ljb/rxjava/fragment/TimerFragment.java
+++ app/src/main/java/com/che58/ljb/rxjava/fragment/TimerFragment.java
@@ -1,6 +1,6 @@
 package com.che58.ljb.rxjava.fragment;
 
-import android.animation.ObjectAnimator;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -10,6 +10,8 @@ import android.widget.ImageView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.che58.ljb.rxjava.R;
+import com.che58.ljb.rxjava.utils.XgoLog;
+import com.trello.rxlifecycle.android.FragmentEvent;
 import com.trello.rxlifecycle.components.support.RxFragment;
 import java.util.concurrent.TimeUnit;
 import rx.Observable;
@@ -24,23 +26,76 @@ public class TimerFragment extends RxFragment {
 
   @BindView(R.id.iv_welcome) ImageView iv_welcome;
 
+  @Override public void onAttach(Activity activity) {
+    super.onAttach(activity);
+    XgoLog.d("onAttach()");
+  }
+
   @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_timer, null);
     ButterKnife.bind(this, view);
+    XgoLog.d("onCreateView()");
     return view;
+  }
+
+  @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    XgoLog.d("onViewCreated()");
   }
 
   @Override public void onActivityCreated(@Nullable Bundle savedInstanceState) {
     super.onActivityCreated(savedInstanceState);
     starTimer();
+    XgoLog.d("onActivityCreated()");
+  }
+
+  @Override public void onStart() {
+    super.onStart();
+    XgoLog.d("onStart()");
+  }
+
+  @Override public void onResume() {
+    super.onResume();
+    XgoLog.d("onResume()");
+  }
+
+  @Override public void onPause() {
+    super.onPause();
+    XgoLog.d("onPause()");
+  }
+
+  @Override public void onStop() {
+    super.onStop();
+    XgoLog.d("onStop()");
+  }
+
+  @Override public void onDestroyView() {
+    super.onDestroyView();
+    XgoLog.d("onDestroyView()");
+  }
+
+  @Override public void onDestroy() {
+    super.onDestroy();
+    XgoLog.d("onDestroy()");
+  }
+
+  @Override public void onDetach() {
+    super.onDetach();
+    XgoLog.d("onDetach()");
   }
 
   private void starTimer() {
-    Observable.timer(3000, TimeUnit.MILLISECONDS).observeOn(AndroidSchedulers.mainThread()).compose(this.<Long>bindToLifecycle()).subscribe(new Action1<Long>() {
+    //Observable.timer(3000, TimeUnit.MILLISECONDS).observeOn(AndroidSchedulers.mainThread()).compose(this.<Long>bindToLifecycle()).subscribe(new Action1<Long>() {
+    //  @Override public void call(Long aLong) {
+    //    // Glide.with(TimerFragment.this).load("http://static.zuchecdn.com/wap/newversion/images/20151225fanli_app.jpg").crossFade().into(iv_welcome);
+    //    iv_welcome.setVisibility(View.VISIBLE);
+    //    ObjectAnimator.ofFloat(iv_welcome, "alpha", 0.0F, 1.0F).setDuration(500).start();
+    //  }
+    //});
+    Observable.interval(100, TimeUnit.MILLISECONDS).observeOn(AndroidSchedulers.mainThread()).compose(this.<Long>bindUntilEvent(FragmentEvent.DESTROY)).subscribe(new Action1<Long>() {
+      //Observable.timer(1, TimeUnit.SECONDS).observeOn(AndroidSchedulers.mainThread()).compose(this.<Long>bindUntilEvent(FragmentEvent.PAUSE)).subscribe(new Action1<Long>() {
       @Override public void call(Long aLong) {
-        // Glide.with(TimerFragment.this).load("http://static.zuchecdn.com/wap/newversion/images/20151225fanli_app.jpg").crossFade().into(iv_welcome);
-        iv_welcome.setVisibility(View.VISIBLE);
-        ObjectAnimator.ofFloat(iv_welcome, "alpha", 0.0F, 1.0F).setDuration(500).start();
+        XgoLog.d("--------------");
       }
     });
   }
--- app/src/main/java/com/che58/ljb/rxjava/utils/XgoLog.java
+++ app/src/main/java/com/che58/ljb/rxjava/utils/XgoLog.java
@@ -4,184 +4,163 @@ import android.util.Log;
 
 public final class XgoLog {
 
-    /**
-     * 是否允许输出log
-     * -1：  不允许
-     * 其他：根据等级允许
-     */
-    private static int mDebuggable = 10;
+  /**
+   * 是否允许输出log
+   * -1：  不允许
+   * 其他：根据等级允许
+   */
+  private static int mDebuggable = 10;
 
-    /**
-     * 日志输出时的TAG
-     */
+  /**
+   * 日志输出时的TAG
+   */
 
-    private static String mTag = "xgo";
+  private static String mTag = "xgo";
 
+  /**
+   * 日志输出级别NONE
+   */
+  public static final int LEVEL_NONE = 0;
 
-    /**
-     * 日志输出级别NONE
-     */
-    public static final int LEVEL_NONE = 0;
+  /**
+   * 日志输出级别V
+   */
 
+  public static final int LEVEL_VERBOSE = 1;
 
-    /**
-     * 日志输出级别V
-     */
+  /**
+   * 日志输出级别D
+   */
 
-    public static final int LEVEL_VERBOSE = 1;
+  public static final int LEVEL_DEBUG = 2;
 
-    /**
-     * 日志输出级别D
-     */
+  /**
+   * 日志输出级别I
+   */
 
-    public static final int LEVEL_DEBUG = 2;
+  public static final int LEVEL_INFO = 3;
 
-    /**
-     * 日志输出级别I
-     */
+  /**
+   * 日志输出级别W
+   */
 
-    public static final int LEVEL_INFO = 3;
+  public static final int LEVEL_WARN = 4;
 
-    /**
-     * 日志输出级别W
-     */
+  /**
+   * 日志输出级别E
+   */
 
-    public static final int LEVEL_WARN = 4;
+  public static final int LEVEL_ERROR = 5;
 
-    /**
-     * 日志输出级别E
-     */
+  private XgoLog() throws InstantiationException {
+    throw new InstantiationException("This class is not created for instantiation");
+  }
 
-    public static final int LEVEL_ERROR = 5;
+  /**
+   * 以级别为v 的形式输出LOG
+   */
 
-    private XgoLog() throws InstantiationException {
-        throw new InstantiationException("This class is not created for instantiation");
+  public static void v(String msg) {
+
+    if (mDebuggable >= LEVEL_VERBOSE) {
+
+      Log.v(mTag, msg);
     }
+  }
 
-    /**
-     * 以级别为v 的形式输出LOG
-     */
+  /**
+   * 以级别为 d 的形式输出LOG
+   */
 
-    public static void v(String msg) {
+  public static void d(String msg) {
 
-        if (mDebuggable >= LEVEL_VERBOSE) {
+    if (mDebuggable >= LEVEL_DEBUG) {
 
-            Log.v(mTag, msg);
-
-        }
-
+      Log.d(mTag, msg);
     }
+  }
 
-    /**
-     * 以级别为 d 的形式输出LOG
-     */
+  /**
+   * 以级别为 i 的形式输出LOG
+   */
 
-    public static void d(String msg) {
+  public static void i(String msg) {
 
-        if (mDebuggable >= LEVEL_DEBUG) {
+    if (mDebuggable >= LEVEL_INFO) {
 
-            Log.d(mTag, msg);
-
-        }
-
+      Log.i(mTag, msg);
     }
+  }
 
-    /**
-     * 以级别为 i 的形式输出LOG
-     */
+  /**
+   * 以级别为 w 的形式输出LOG
+   */
 
-    public static void i(String msg) {
+  public static void w(String msg) {
 
-        if (mDebuggable >= LEVEL_INFO) {
+    if (mDebuggable >= LEVEL_WARN) {
 
-            Log.i(mTag, msg);
-
-        }
-
+      Log.w(mTag, msg);
     }
+  }
 
-    /**
-     * 以级别为 w 的形式输出LOG
-     */
+  /**
+   * 以级别为 w 的形式输出Throwable
+   */
 
-    public static void w(String msg) {
+  public static void w(Throwable tr) {
 
-        if (mDebuggable >= LEVEL_WARN) {
+    if (mDebuggable >= LEVEL_WARN) {
 
-            Log.w(mTag, msg);
-
-        }
-
+      Log.w(mTag, "", tr);
     }
+  }
 
-    /**
-     * 以级别为 w 的形式输出Throwable
-     */
+  /**
+   * 以级别为 w 的形式输出LOG信息和Throwable
+   */
 
-    public static void w(Throwable tr) {
+  public static void w(String msg, Throwable tr) {
 
-        if (mDebuggable >= LEVEL_WARN) {
+    if (mDebuggable >= LEVEL_WARN && null != msg) {
 
-            Log.w(mTag, "", tr);
-
-        }
-
+      Log.w(mTag, msg, tr);
     }
+  }
 
-    /**
-     * 以级别为 w 的形式输出LOG信息和Throwable
-     */
+  /**
+   * 以级别为 e 的形式输出LOG
+   */
 
-    public static void w(String msg, Throwable tr) {
+  public static void e(String msg) {
 
-        if (mDebuggable >= LEVEL_WARN && null != msg) {
+    if (mDebuggable >= LEVEL_ERROR) {
 
-            Log.w(mTag, msg, tr);
-
-        }
-
+      Log.e(mTag, msg);
     }
+  }
 
-    /**
-     * 以级别为 e 的形式输出LOG
-     */
+  /**
+   * 以级别为 e 的形式输出Throwable
+   */
 
-    public static void e(String msg) {
+  public static void e(Throwable tr) {
 
-        if (mDebuggable >= LEVEL_ERROR) {
+    if (mDebuggable >= LEVEL_ERROR) {
 
-            Log.e(mTag, msg);
-
-        }
-
+      Log.e(mTag, "", tr);
     }
+  }
 
-    /**
-     * 以级别为 e 的形式输出Throwable
-     */
+  /**
+   * 以级别为 e 的形式输出LOG信息和Throwable
+   */
 
-    public static void e(Throwable tr) {
+  public static void e(String msg, Throwable tr) {
 
-        if (mDebuggable >= LEVEL_ERROR) {
+    if (mDebuggable >= LEVEL_ERROR && null != msg) {
 
-            Log.e(mTag, "", tr);
-
-        }
-
+      Log.e(mTag, msg, tr);
     }
-
-    /**
-     * 以级别为 e 的形式输出LOG信息和Throwable
-     */
-
-    public static void e(String msg, Throwable tr) {
-
-        if (mDebuggable >= LEVEL_ERROR && null != msg) {
-
-            Log.e(mTag, msg, tr);
-
-        }
-
-    }
-
+  }
 }
